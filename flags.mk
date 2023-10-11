88109b2c185b530c878199642d8abe5c9bfe50a7
Emma Ericsson <emma@krlsg.se>
2023-10-11 14:24:36 +0200

    Read input from file

--- flags.mk
+++ flags.mk
@@ -1,7 +1,7 @@
 CC=gcc
 
 CPPFLAGS?=
-CPPFLAGS+=-Iinclude
+CPPFLAGS+=-Iinclude -D_POSIX_C_SOURCE=2
 
 CFLAGS?=-O2 -g
 CFLAGS+=-Wall -Wextra -MD -std=c99
--- src/main.c
+++ src/main.c
@@ -1,12 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <parser/token.h>
 #include <parser/lexer.h>
 
-int main(void) {
-    const char str[] = "let hej = 56 == 5 | 6 && 3;";
-    lexer_t *lexer = lexer_create(str, sizeof str - 1);
+#define PROGRAM_NAME "compiler"
+
+const char *help_str = ""
+"Usage: "PROGRAM_NAME" [option]... infile\n"
+"\n"
+"  -h            print this help message\n"
+"  -o outfile    output program to outfile\n"
+;
+
+struct options {
+    const char *infile, *outfile;
+} options;
+
+int main(int argc, char *argv[]) {
+    int ret = EXIT_SUCCESS;
+
+    options = (struct options){
+        .outfile = "./a.out",
+    };
+
+    int c;
+    while((c = getopt(argc, argv, "ho:")) != -1) {
+        switch(c) {
+        case 'o':
+            options.outfile = optarg;
+            break;
+
+        default:
+            fprintf(stderr, "%s", help_str);
+            exit(EXIT_FAILURE);
+        }
+    }
+    argc -= optind, argv += optind;
+
+    if(argc < 1) {
+        fprintf(stderr, "%s", help_str);
+        exit(EXIT_FAILURE);
+    }
+    options.infile = argv[0];
+
+    FILE *f;
+    if(!(f = fopen(options.infile, "r"))) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(f, 0, SEEK_END);
+    size_t sz = ftell(f);
+    rewind(f);
+    char *buf = malloc(sz);
+    if(fread(buf, 1, sz, f) != sz) {
+        fprintf(stderr, "Failed to read entire file\n");
+        fclose(f);
+        ret = EXIT_FAILURE;
+        goto ret_free;
+    }
+    fclose(f);
+
+    lexer_t *lexer = lexer_create(buf, sz - 1);
 
     for(;;) {
         token_t *token = lexer_next(lexer);
@@ -23,6 +80,7 @@ int main(void) {
     }
 
     lexer_destroy(lexer);
-
-    return EXIT_SUCCESS;
+ret_free:
+    free(buf);
+    exit(ret);
 }
